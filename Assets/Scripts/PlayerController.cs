9e67432d58d53f819899d2481f0ea96e2f5aace2
Rommel Rodrigues de Oliveira <rommel.osmiun@gmail.com>
2020-10-15 00:37:12 -0700

    Import prototyping assets; Set up game scene; Implement player movement and animations.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 moveDir;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float xSpeed;
+    [SerializeField] private float ySpeed;
+    private Rigidbody2D rb2D;
+    private Animator animator;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(moveDir);
+    }
+
+    private void MovePlayer(Vector2 moveDir)
+    {
+        rb2D.MovePosition(rb2D.position + moveDir * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void HandleInput()
+    {
+        moveDir.x = Input.GetAxisRaw("Horizontal");
+        moveDir.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("horizontal_input", moveDir.x);
+        animator.SetFloat("vertical_input", moveDir.y);
+    }
+}
